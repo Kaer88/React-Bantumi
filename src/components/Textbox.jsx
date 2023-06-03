491c790f59a0c6ed80774aee56dbd6cd41cd81dd
Kaer88 <robert.koncz88@gmail.com>
2023-06-03 21:06:42 +0200

    minor text fix

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -17,6 +17,15 @@ export default function Textbox() {
                 <>
                     <div>
                         <h1>Mancala - Bantumi</h1>
+                        <h4>Játékostábla:</h4>
+                        <ul>
+                            <li>
+                                1. Játékos: Alsó térfél
+                            </li>
+                            <li>
+                                2. Játékos: Felső térfél
+                            </li>
+                        </ul>
                         <h4>Szabályok:</h4>
                         <ul>
 
@@ -24,25 +33,25 @@ export default function Textbox() {
                                 A játékosok felváltva választanak az előttük lévő 6 tálból, melyekben 4-4 bab van. 
                             </li>
                             <li>
-                                A kiválasztott tálból a játékos kiveszi a babokat és egyesével leteszi a következő tálakba az óramutató járásával ellentétesen haladva, míg a bab el nem fogy a kézből
+                                A kiválasztott tálból a játékos kiveszi a babokat és egyesével leteszi a következő tálakba, körben az óramutató járásával ellentétesen haladva, míg a bab el nem fogy a kézből.
                             </li>
                             <li>
-                                A játékosok jobb oldalán helyezkedik el a gyűjtőedény. A babok letétele során a saját gyüjtőedénybe is teszünk egy babot, de az ellenfél gyűjtőedényét kihagyjuk!
+                                A játékosok jobb oldalán helyezkedik el a nagyobb gyűjtőedény. A babok letétele során a saját gyüjtőedénybe is teszünk egy babot, de az ellenfél gyűjtőedényét kihagyjuk!
                             </li>
                             <li>
-                                Ha az utolsó letett bab saját oldali üres tálba kerül, akkor azt, illetve az ellenfél szemben lévő táljából az összes babot a saját eredménytálunkba tehetjük
+                                Ha az utolsó letett bab saját oldali üres tálba kerül, akkor azt, illetve az ellenfél szemben lévő táljából az összes babot a saját eredménytálunkba tehetjük. Ez a Rablás!
                             </li>
                             <li>
-                                Ha az utolsó letett bab a játékos saját eredménytáljába kerül, akkor még egyszer ugyanaz a játékos jön
+                                Ha az utolsó letett bab a játékos saját gyűjtőedényébe kerül, még egyszer ugyanaz a játékos jön.
                             </li>
                             <li>
-                                Minden egyéb esetben a következő játékos kerül sorra
+                                Minden egyéb esetben a következő játékos kerül sorra.
                             </li>
                         </ul>
                         <h4>Játék vége:</h4>
                         <ul>
                             <li>
-                                Ha bármelyik oldalon lévő tálakból elfogy az összes bab, vége a játéknak
+                                Ha bármelyik oldalon lévő tálakból elfogy az összes bab, vége a játéknak.
                             </li>
                             <li>
                                 A másik térfélen megmaradt babokat annak a játékosnak az eredménytálába tesszük, amelyik oldalon vannak a megmaradt babok.
