eb4319ace67bd8d7ffc6e897868d2d33220241a0
Kaer88 <robert.koncz88@gmail.com>
2023-06-14 21:25:08 +0200

    fix pot count logic

--- src/components/Pot.jsx
+++ src/components/Pot.jsx
@@ -6,21 +6,25 @@ export default function Pot({ beans, onClick, className, delay }) {
     const [animationState, setAnimationState] = useState()
 
     useEffect(() => {
-        setNrOfBeans(beans)
         if (beans != 0) {
 
             setTimeout(() => {
                 setAnimationState(true);
                 setTimeout(() => {
                     setAnimationState(false)
+                    
+
                 }, 300)
+                setNrOfBeans(beans)
             }, delay)
+        } else {
+            setNrOfBeans(beans)
         }
     }, [beans])
 
     return (
         <div className={className} onClick={onClick}>
-            <p>{animationState && nrOfBeans !=0 ? nrOfBeans - 1 : nrOfBeans}</p>
+            <p>{animationState && nrOfBeans != 0 ? nrOfBeans - 1 : nrOfBeans}</p>
             {animationState && <Hand />}
         </div>
     )
